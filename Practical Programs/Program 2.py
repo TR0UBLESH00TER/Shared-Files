9c5753733c998250ccbfe9e489f75cc1b33d1429
sagamantus <74814583+sagamantus@users.noreply.github.com>
2021-03-24 17:00:00 +0530

    Update Program 2.py

--- Practical Programs/Program 2.py
+++ Practical Programs/Program 2.py
@@ -1,18 +1,18 @@
 # 2. WAF to accept a number from user and to check whether it is Prime or not
 def Check_Prime(number):
     """Returns True if prime and False if not prime"""
-    if number == 1:
-        return False # 1 is Neither Prime nor Composite
-    elif number in (2,3):
-        return True
+    if number < 2:
+        return False
+
     else:
-        for i in range(2,int((number**0.5))+1):
-            if number % i == 0:
+        for factor in range(2,int((number)**0.5)+1):
+            if number % factor == 0:
                 return False
-                break
         else:
             return True
 
 # Invoking function
 print(Check_Prime(123))
 print(Check_Prime(1279))
+print(CheckPrime(15485863))
+print(CheckPrime(153485863))
